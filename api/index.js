e5f004e51ef2230598c4480a158dda53ce1bf889
Avashbhandari01 <avash115@gmail.com>
2024-05-25 20:25:41 +0545

    Created CRUD Apis for hotels and added error handling middleware

---
+++ api/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const roomsRoute = require("./routes/rooms");
+const hotelsRoute = require("./routes/hotels");
+const app = express();
+require('dotenv').config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log(`Connected to MongoDB!`)
+    } catch (error) {
+        throw (error);
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log(`MongoDB Disconnected!`)
+})
+
+// middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.Status || 500;
+    const errorMessage = err.Message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    connect()
+    console.log(`Server is running on port: ${process.env.PORT}`);
+})
