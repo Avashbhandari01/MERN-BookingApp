c4e2e76459e9ab4e063866fed3af765e2d77adf7
Avashbhandari01 <avash115@gmail.com>
2024-05-31 10:03:28 +0545

    Footer Design

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footerLists">
+        <ul className="footerList">
+          <li className="footerListItem">Countries</li>
+          <li className="footerListItem">Regions</li>
+          <li className="footerListItem">Cities</li>
+          <li className="footerListItem">Districts</li>
+          <li className="footerListItem">Airports</li>
+          <li className="footerListItem">Hotels</li>
+        </ul>
+        <ul className="footerList">
+          <li className="footerListItem">Homes</li>
+          <li className="footerListItem">Apartments</li>
+          <li className="footerListItem">Resorts</li>
+          <li className="footerListItem">Villas</li>
+          <li className="footerListItem">Hostels</li>
+          <li className="footerListItem">B&Bs</li>
+        </ul>
+        <ul className="footerList">
+          <li className="footerListItem">Unique places to stay</li>
+          <li className="footerListItem">All destinations</li>
+          <li className="footerListItem">All flight destinations</li>
+          <li className="footerListItem">All car rental locations</li>
+          <li className="footerListItem">All vacation destinations</li>
+          <li className="footerListItem">Reviews</li>
+        </ul>
+        <ul className="footerList">
+          <li className="footerListItem">Car rental</li>
+          <li className="footerListItem">Flight finder</li>
+          <li className="footerListItem">Restaurant reservations</li>
+          <li className="footerListItem">Seasonal and holiday deals</li>
+          <li className="footerListItem">Traveller Review Awards</li>
+          <li className="footerListItem">Booking.com for Travel Agents</li>
+        </ul>
+        <ul className="footerList">
+          <li className="footerListItem">Coronavirus (COVID-19) FAQs</li>
+          <li className="footerListItem">About Booking.com</li>
+          <li className="footerListItem">Customer Service Help</li>
+          <li className="footerListItem">Partner help</li>
+          <li className="footerListItem">Careers</li>
+          <li className="footerListItem">Sustainability</li>
+        </ul>
+      </div>
+      <div className="footerText">Copyright © 1996-2024 booking.com</div>
+    </div>
+  );
+}
+
+export default Footer;
