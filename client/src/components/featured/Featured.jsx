e31cc0ba356e929a057d028d103f65d9be937c3e
Avashbhandari01 <avash115@gmail.com>
2024-06-10 20:50:15 +0545

    React slider

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -15,35 +15,35 @@ function Featured() {
         <>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              src="https://cf.bstatic.com/xdata/images/city/600x600/977237.jpg?k=6d894f6f1bc6d83e5206ee19d4e593b008640ec596ed3803071cc03aff856b8b&o="
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
               <h1>Berlin</h1>
-              <h2>{data[0]} properties</h2>
+              {/* <h2>{data[0]} properties</h2> */}
             </div>
           </div>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              src="https://cf.bstatic.com/xdata/images/city/600x600/971353.jpg?k=a5b47d2c38469086427837dfe64ab2e6f57ba52f0504359ef4cc02dc4b9b0737&o="
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
               <h1>Madrid</h1>
-              <h2>{data[1]} properties</h2>
+              {/* <h2>{data[1]} properties</h2> */}
             </div>
           </div>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              src="https://cf.bstatic.com/xdata/images/city/600x600/977262.jpg?k=2b852648c76ccaff8be05333057712eda873343dfaa79cd23e55534a1a55aecc&o="
               alt=""
               className="featuredImg"
             />
             <div className="featuredTitles">
               <h1>London</h1>
-              <h2>{data[2]} properties</h2>
+              {/* <h2>{data[2]} properties</h2> */}
             </div>
           </div>
         </>
--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,42 +1,67 @@
 import "./propertyList.css";
 import useFetch from "../../hooks/useFetch";
 import Skeleton from "../skeleton/Skeleton";
+import Slider from "react-slick";
 
 function PropertyList() {
   const { data, loading, error } = useFetch(
-    "http://localhost:8800/api/hotels/countByType"
+    "http://localhost:8800/api/propertyType"
   );
 
-  const images = [
-    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
-  ];
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className="propertyList">
       {loading ? (
-        <Skeleton type="property" />
+        "Loading please wait..."
       ) : (
         <>
-          {data &&
-            images.map((img, i) => (
-              <div className="propertyListItem" key={i}>
+          <Slider {...settings}>
+            {data.map((item) => (
+              <div className="propertyListItem" key={item._id}>
                 <img
-                  src={img}
+                  src={item.image}
                   alt="Property Images"
                   className="propertyListImage"
                 />
                 <div className="propertyListTitles">
-                  <h1>{data[i]?.type}</h1>
-                  <h2>
-                    {data[i]?.count} {data[i]?.type}
-                  </h2>
+                  <h1>{item.name}</h1>
                 </div>
               </div>
             ))}
+          </Slider>
         </>
       )}
     </div>
